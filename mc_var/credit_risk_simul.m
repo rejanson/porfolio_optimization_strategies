083b013988ba65380adf326bb95b305ef1d1a565
RJ <wzr@live.dk>
2018-03-18 22:46:13 -0400

    added credit risk monte carlo simulation

--- mc_var/credit_risk_simul.m
+++ mc_var/credit_risk_simul.m
@@ -48,13 +48,42 @@ exposure(:, 1) = (1-recov_rate) .* exposure(:, 1);
 % Compute credit-state boundaries
 CS_Bdry = norminv( cumsum(prob(:,1:C-1), 2) );
 
-% TODO: -------- Insert your code here -------- %
+%% TODO: -------- Insert your code here -------- %
 
 if(~exist('scenarios_out.mat','file'))
     
     % TODO: -------- Insert your code here -------- %
+    Losses_out = zeros(Nout, K);
+    
+    
+    parfor s = 1:Nout
+        if mod(s, 1000) == 0
+           s 
+        end
+        loss = [];
+        zscores = mvnrnd(zeros(1,50) , rho);
+        
+        for i = 1:size(beta, 1)
+            sigma_i = sqrt(1 - beta(i)^2);
+            beta_i = beta(i);
+            z_stat = beta_i * zscores(driver(i)) + sigma_i * mvnrnd(0, 1);
+            
+            for credit_state_i = 1:size(CS_Bdry, 2)
+                current_boundary = CS_Bdry(i, credit_state_i);
+                if z_stat < current_boundary
+                    loss = [loss exposure(i, credit_state_i)];
+                    break;
+                end
+                
+                if credit_state_i >= 7
+                    loss = [loss exposure(i, credit_state_i)];
+                end
+            end
+            
+        end
 
-    for s = 1:Nout
+        Losses_out(s, :) = loss;
+
         % TODO: -------- Insert your code here -------- %
     end
 
@@ -80,35 +109,85 @@ x0{2} = (portf_v ./ value) .* w0{2};  % asset units (portfolio 2)
 % Quantile levels (99%, 99.9%)
 alphas = [0.99 0.999];
 
+VaRout = zeros(2, length(alphas));
+VaRinN = zeros(2, length(alphas));
+CVaRout = zeros(2, length(alphas));
+CVaRinN = zeros(2, length(alphas));
+
 % Compute VaR and CVaR (non-Normal and Normal) for 100000 scenarios
 for(portN = 1:2)
+    losses = Losses_out * cell2mat(x0(portN));
+    losses = sort(losses);
     for(q=1:length(alphas))
         alf = alphas(q);
-        % TODO:
+        
         % -------- Insert your code here -------- %
-        % VaRout(portN,q)  = ...
-        % VaRinN(portN,q)  = ...
-        % CVaRout(portN,q) = ...
-        % CVaRinN(portN,q) = ...
+        VaRout(portN,q)  = losses(ceil((Nout-1) * alf));
+        VaRinN(portN,q)  = mu_l' * cell2mat(x0(portN)) + norminv(alf,0,1)*std(losses);
+        CVaRout(portN,q) = (1/((Nout-9)*(1-alf))) *  ...
+                ((ceil((Nout-9)*alf)-(Nout-9)*alf) * VaRout(portN,q) + ...
+                sum(losses(ceil((Nout-9)*alf)+1:Nout-9)));   
+        CVaRinN(portN,q) = mu_l' * cell2mat(x0(portN)) + ...
+            (normpdf(norminv(alf,0,1))/(1-alf))*std(losses);
         % -------- Insert your code here -------- %        
  end
 end
 
 
-% Perform 100 trials
-N_trials = 100;
+%% Perform 100 trials
+N_trials = 10;
+
+VaRinMC1 = {};
+VaRinMC2 = {};
+VaRinN1 = {};
+VaRinN2 = {};
+CVaRinMC1 = {};
+CVaRinMC2 = {};
+CVaRinN1 = {};
+CVaRinN2 = {};
+
+Cumulative_MC1 = [];
+Cumulative_MC2 = [];
 
 for(tr=1:N_trials)
-    % TODO:
     % Monte Carlo approximation 1
 
     % -------- Insert your code here -------- %
+    Losses_inMC1 = zeros(Nin, K);
     
     for s = 1:ceil(Nin/Ns) % systemic scenarios
         % -------- Insert your code here -------- %
+        
+        zscores = mvnrnd(zeros(1,50) , rho);
+
         for si = 1:Ns % idiosyncratic scenarios for each systemic
             % -------- Insert your code here -------- %
+            loss = [];
+            
+            for i = 1:size(beta, 1)
+                sigma_i = sqrt(1 - beta(i)^2);
+                beta_i = beta(i);
+                z_stat = beta_i * zscores(driver(i)) + sigma_i * mvnrnd(0, 1);
+
+                for credit_state_i = 1:size(CS_Bdry, 2)
+                    current_boundary = CS_Bdry(i, credit_state_i);
+                    if z_stat < current_boundary
+                        loss = [loss exposure(i, credit_state_i)];
+                        break;
+                    end
+
+                    if credit_state_i >= 7
+                        loss = [loss exposure(i, credit_state_i)];
+                    end
+                end
+
+            end
+
+            
+            Losses_inMC1((s - 1) * si + si, :) = loss;
         end
+        
+
     end
     
     % Calculated losses for MC1 approximation (5000 x 100)
@@ -117,9 +196,36 @@ for(tr=1:N_trials)
     % Monte Carlo approximation 2
     
     % -------- Insert your code here -------- %
+    Losses_inMC2 = zeros(Nin, K);
     
-    for s = 1:Nin % systemic scenarios (1 idiosyncratic scenario for each systemic)
+    parfor s = 1:Nin % systemic scenarios (1 idiosyncratic scenario for each systemic)
         % -------- Insert your code here -------- %
+        zscores = mvnrnd(zeros(1,50) , rho);
+        
+        loss = [];
+            
+        for i = 1:size(beta, 1)
+            sigma_i = sqrt(1 - beta(i)^2);
+            beta_i = beta(i);
+            z_stat = beta_i * zscores(driver(i)) + sigma_i * mvnrnd(0, 1);
+
+            for credit_state_i = 1:size(CS_Bdry, 2)
+                current_boundary = CS_Bdry(i, credit_state_i);
+                if z_stat < current_boundary
+                    loss = [loss exposure(i, credit_state_i)];
+                    break;
+                end
+
+                if credit_state_i >= 7
+                    loss = [loss exposure(i, credit_state_i)];
+                end
+            end
+
+        end
+
+
+        Losses_inMC2(s, :) = loss;
+
     end
         
     % Calculated losses for MC2 approximation (5000 x 100)
@@ -131,8 +237,8 @@ for(tr=1:N_trials)
             alf = alphas(q);
             % -------- Insert your code here -------- %            
             % Compute portfolio loss 
-            % portf_loss_inMC1 = ...
-            % portf_loss_inMC2 = ...
+            portf_loss_inMC1 = sort(Losses_inMC1 * cell2mat(x0(portN)));
+            portf_loss_inMC2 = sort(Losses_inMC2 * cell2mat(x0(portN)));
             mu_MCl = mean(Losses_inMC1)';
             var_MCl = cov(Losses_inMC1);
             mu_MC2 = mean(Losses_inMC2)';
@@ -140,20 +246,33 @@ for(tr=1:N_trials)
             % Compute portfolio mean loss mu_p_MC1 and portfolio standard deviation of losses sigma_p_MC1
             % Compute portfolio mean loss mu_p_MC2 and portfolio standard deviation of losses sigma_p_MC2
             % Compute VaR and CVaR for the current trial
-            % VaRinMC1{portN,q}(tr) = ...
-            % VaRinMC2{portN,q}(tr) = ...
-            % VaRinN1{portN,q}(tr) = ...
-            % VaRinN2{portN,q}(tr) = ...
-            % CVaRinMC1{portN,q}(tr) = ...
-            % CVaRinMC2{portN,q}(tr) = ...
-            % CVaRinN1{portN,q}(tr) = ...
-            % CVaRinN2{portN,q}(tr) = ...
+            VaRinMC1{portN,q}(tr) = portf_loss_inMC1(ceil((Nin-1) * alf));
+            VaRinMC2{portN,q}(tr) = portf_loss_inMC2(ceil((Nin-1) * alf));
+            VaRinN1{portN,q}(tr) = mu_MCl' * cell2mat(x0(portN)) ...
+                    + norminv(alf,0,1)*std(portf_loss_inMC1);
+            VaRinN2{portN,q}(tr) =  mu_MC2' * cell2mat(x0(portN)) ...
+                    + norminv(alf,0,1)*std(portf_loss_inMC2);
+            CVaRinMC1{portN,q}(tr) = (1/((Nin-9)*(1-alf))) *  ...
+                ((ceil((Nin-9)*alf)-(Nin-9)*alf) * VaRinMC1{portN,q}(tr) + ...
+                sum(portf_loss_inMC1(ceil((Nin-9)*alf)+1:Nin-9)));  
+            CVaRinMC2{portN,q}(tr) = (1/((Nin-9)*(1-alf))) *  ...
+                ((ceil((Nin-9)*alf)-(Nin-9)*alf) * VaRinMC2{portN,q}(tr) + ...
+                sum(portf_loss_inMC2(ceil((Nin-9)*alf)+1:Nin-9)));  
+            CVaRinN1{portN,q}(tr) = mu_MCl' * cell2mat(x0(portN)) + ...
+                (normpdf(norminv(alf,0,1))/(1-alf))*std(portf_loss_inMC1);
+            CVaRinN2{portN,q}(tr) = mu_MC2' * cell2mat(x0(portN)) + ...
+                (normpdf(norminv(alf,0,1))/(1-alf))*std(portf_loss_inMC2);
             % -------- Insert your code here -------- %
         end
     end
+    
+    Cumulative_MC1 = [Cumulative_MC1; portf_loss_inMC1];
+    Cumulative_MC2 = [Cumulative_MC2; portf_loss_inMC2];
 end
 
-% Display portfolio VaR and CVaR
+
+
+%% Display portfolio VaR and CVaR
 for(portN = 1:2)
 fprintf('\nPortfolio %d:\n\n', portN)    
  for(q=1:length(alphas))
